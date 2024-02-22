b000365d893d30f416c3b38ce0a06d9f4df17b30
AWNDOYE <awamakele@gmail.com>
2024-02-22 16:41:51 +0000

    CheckPoint_Redux_Toolkit_2

--- src/Componenents/AddTache.js
+++ src/Componenents/AddTache.js
@@ -12,20 +12,24 @@ const AddTache = () => {
     })
 
     const handleAddTask = () => {
-        // {console.log("opp",newTask.description.length)}
-        if (newTask.description.length > 1 ) {
-            //   {console.log(newTask)}  
+        
+        if (newTask.trim() !== '' ) {
+              {console.log(newTask)}  
             dispatch(ajouterUneTache({ id: Date.now(), description: newTask, isDone : newTask}))
             setNewTask('')
-            }else{
-                alert("Veuiller saisir une description")
             }
+    
+        // {console.log(newTask.description)}
+
+        // dispatch(ajouterUneTache({description: newTask, isDone : newTask}))
+        // setNewTask('')
     }
+
     return (
         <div style={{display:'flex', width:'50%', backgroundColor:'goldenrod', height:'50%', margin:'10px', padding:'20px',  marginLeft:'20%',justifyContent:'space-between'}}>
             <label style={{fontSize:'120%', fontWeight:'bold'}}>Nouvelle Tâche</label>
-            <input type='text' placeholder="saisir une nouvelle tâche"  value={newTask.description}  onChange={e => setNewTask(e.target.value)}></input>
-            <input type='boolean'  placeholder='saisir 0 ou 1' value={newTask.isDone} onChange={e =>(e.target.value)}></input>
+            <input type='text' value={newTask.description} placeholder='saisir une nouvelle tâche'  onChange={e => setNewTask(e.target.value)} required/>
+            <input type='number' min={0} max={1} placeholder='saisir 0 ou 1' value={newTask.isDone} onChange={e =>(e.target.value)}></input>
             <button onClick={handleAddTask}> Ajouter une tâche</button>
         </div>
     );
